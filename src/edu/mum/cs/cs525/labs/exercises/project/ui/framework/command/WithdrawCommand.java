3b83a85f649819737fe3c30c5ccb5d0e0e8fa1a0
Kidus Mamuye Tekeste <kidusmamuye@gmail.com>
2021-07-13 08:55:10 -0500

    handled the commands with some incomplete method signatures

--- src/edu/mum/cs/cs525/labs/exercises/project/ui/framework/command/WithdrawCommand.java
+++ src/edu/mum/cs/cs525/labs/exercises/project/ui/framework/command/WithdrawCommand.java
@@ -1,14 +1,23 @@
 package edu.mum.cs.cs525.labs.exercises.project.ui.framework.command;
 
-public class WithdrawCommand implements Command {
+import edu.mum.cs.cs525.labs.exercises.project.ui.framework.model.Account;
+import edu.mum.cs.cs525.labs.exercises.project.ui.framework.model.BaseEntity;
 
+public class WithdrawCommand implements Command {
+    Account account;
+    BaseEntity entity;
+    public WithdrawCommand(Account account, BaseEntity entity){
+        this.account = account;
+        this.entity = entity;
+    }
     @Override
     public void execute() {
-
+        System.out.println("Withdraw money amount "+account.getCurrentBalance());
+//        this.account.setCurrentBalance(entity.getRequestAmount());
     }
 
     @Override
     public void undo() {
-
+        // todo undo operation goes in here
     }
 }
