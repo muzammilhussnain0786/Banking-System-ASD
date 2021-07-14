58e29b4e27fb6706a195486e6dc980075c0b6edc
Muzammil Husnain <muzammil@convo.com>
2021-07-14 12:21:37 -0500

    refactoring and cleanup

--- src/main/java/edu/mum/cs/cs525/project/framework/service/EmailSenderService.java
+++ src/main/java/edu/mum/cs/cs525/project/framework/service/EmailSenderService.java
@@ -1,12 +1,13 @@
 package edu.mum.cs.cs525.project.framework.service;
 
-import edu.mum.cs.cs525.labs.exercises.project.ui.framework.model.account.Account;
+
+import edu.mum.cs.cs525.project.framework.accounts.Account;
 import edu.mum.cs.cs525.project.framework.observer.Observer;
 
 public class EmailSenderService implements Observer<Account> {
     @Override
     public void update(Account data) {
         //TODO think about the template for sending email
-        System.out.println("Account " + data.getId() +" has new balance: " + data.getCurrentBalance());
+        System.out.println("Account " + data.getAccountNumber() +" has new balance: " + data.getBalance());
     }
 }
