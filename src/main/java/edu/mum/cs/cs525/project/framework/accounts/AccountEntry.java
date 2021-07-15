ed0dacd8ab8e8592b38c5030f525c9e158b7173b
Quoc Trinh <quoc.trinh@outlook.com>
2021-07-15 07:29:39 -0500

    Add account statement

--- src/main/java/edu/mum/cs/cs525/project/framework/accounts/AccountEntry.java
+++ src/main/java/edu/mum/cs/cs525/project/framework/accounts/AccountEntry.java
@@ -1,65 +1,72 @@
 package edu.mum.cs.cs525.project.framework.accounts;
 
+import edu.mum.cs.cs525.project.framework.uitoolkit.TableRow;
+
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class AccountEntry implements Serializable {
-	private Date date;
-	private double amount;
-	private String description;
-	private String fromAccountNumber;
-	private String fromPersonName;
-	
-	public AccountEntry() {
-	}
+public class AccountEntry implements Serializable, TableRow {
+    private Date date;
+    private double amount;
+    private String description;
+    private String fromAccountNumber;
+    private String fromPersonName;
 
-	public AccountEntry(double amount, String description, String fromAccountNumber, String fromPersonName) {
-		super();
-		this.date = new Date();
-		this.amount = amount;
-		this.description = description;
-		this.fromAccountNumber = fromAccountNumber;
-		this.fromPersonName = fromPersonName;
-	}
+    public AccountEntry() {
+    }
 
-	public double getAmount() {
-		return amount;
-	}
+    public AccountEntry(double amount, String description, String fromAccountNumber, String fromPersonName) {
+        super();
+        this.date = new Date();
+        this.amount = amount;
+        this.description = description;
+        this.fromAccountNumber = fromAccountNumber;
+        this.fromPersonName = fromPersonName;
+    }
 
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
+    public double getAmount() {
+        return amount;
+    }
 
-	public Date getDate() {
-		return date;
-	}
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
+    public Date getDate() {
+        return date;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public String getFromAccountNumber() {
-		return fromAccountNumber;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public void setFromAccountNumber(String fromAccountNumber) {
-		this.fromAccountNumber = fromAccountNumber;
-	}
+    public String getFromAccountNumber() {
+        return fromAccountNumber;
+    }
 
-	public String getFromPersonName() {
-		return fromPersonName;
-	}
+    public void setFromAccountNumber(String fromAccountNumber) {
+        this.fromAccountNumber = fromAccountNumber;
+    }
 
-	public void setFromPersonName(String fromPersonName) {
-		this.fromPersonName = fromPersonName;
-	}
-	
+    public String getFromPersonName() {
+        return fromPersonName;
+    }
+
+    public void setFromPersonName(String fromPersonName) {
+        this.fromPersonName = fromPersonName;
+    }
+
+    @Override
+    public Object[] row() {
+        return new String[]{new SimpleDateFormat("yyyy-MM-dd HH:mm").format(this.date), amount + "", amount < 0 ? "W" : "D"};
+    }
 }
